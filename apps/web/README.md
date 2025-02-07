329d4606366bc94becd461097fe94ac4ced7d8c9
Alang Kim <rlaangh77@gmail.com>
2025-02-07 20:45:41 +0900

    config: 프론트 web 개발환경 설정

---
+++ apps/web/README.md
@@ -0,0 +1,11 @@
+# Web
+
+API 서버에서 제공하는 데이터를 화면에 렌더링하는 프론트 웹입니다.
+
+## 핵심 기술 스택
+
+- Vite
+- React
+- TypeScript
+- TailwindCSS
+- React Router
---
+++ apps/web/src/App.tsx
@@ -0,0 +1,11 @@
+import "./global.css";
+
+function App() {
+  return (
+    <>
+      <div className="text-xl font-bold">Hello World!</div>
+    </>
+  );
+}
+
+export default App;
