a243b1b39a811c640a63a794a86bc95ba091d45d
Alang Kim <rlaangh77@gmail.com>
2025-02-08 18:34:36 +0900

    test: 테스트 코드 수정

--- apps/api/src/app.service.spec.ts
+++ apps/api/src/app.service.spec.ts
@@ -1,6 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppService } from './app.service';
 import { RiotService } from './riot/riot.service';
+import { CURRENT_GAME_RESPONSE_MOCK } from './__mocks__/current-game-response.mock';
+import { RIOT_SPECTATOR_RESPONSE_MOCK } from './__mocks__/riot-spectator-response.mock';
 
 describe('AppService', () => {
   let service: AppService;
@@ -32,33 +34,19 @@ describe('AppService', () => {
     it('소환사의 gameName과 tagLine을 받아 현재 게임을 찾을 수 있어야 한다.', async () => {
       const mockGameName = 'test-gameName';
       const mockTagLine = 'test-tagLine';
-      const mockCurrentGame = {
-        gameId: 1234567890,
-        mapId: 1234567890,
-        gameMode: 'test-gameMode',
-        gameType: 'test-gameType',
-        gameQueueConfigId: 1234567890,
-        participants: [],
-        observers: {
-          encryptionKey: 'test-encryptionKey',
-        },
-        platformId: 'test-platformId',
-        bannedChampions: [],
-        gameStartTime: 1234567890,
-        gameLength: 1234567890,
-      };
-
       mockRiotService.searchSummoner.mockResolvedValue({
         puuid: 'test-puuid',
       });
 
-      mockRiotService.checkCurrentGame.mockResolvedValue(mockCurrentGame);
+      mockRiotService.checkCurrentGame.mockResolvedValue(
+        RIOT_SPECTATOR_RESPONSE_MOCK,
+      );
       const result = await service.checkCurrentGame({
         gameName: mockGameName,
         tagLine: mockTagLine,
       });
 
-      expect(result).toEqual(mockCurrentGame);
+      expect(result).toEqual(CURRENT_GAME_RESPONSE_MOCK);
     });
   });
 });
